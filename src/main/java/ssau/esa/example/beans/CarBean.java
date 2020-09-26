b86fb7141fd10c7687675fefe47cdf5853cd4c1c
pasha9819 <pasha9819@gmail.com>
2020-09-26 11:27:59 +0400

    CarBean: Stateless -> Singleton

--- src/main/java/ssau/esa/example/beans/CarBean.java
+++ src/main/java/ssau/esa/example/beans/CarBean.java
@@ -2,12 +2,12 @@ package ssau.esa.example.beans;
 
 import ssau.esa.example.entity.Car;
 
-import javax.ejb.Stateless;
+import javax.ejb.Singleton;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Collection;
 
-@Stateless
+@Singleton
 public class CarBean {
 
     @PersistenceContext(unitName = "esa")
